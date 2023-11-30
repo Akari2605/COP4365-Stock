6b572a398ef3637a02c98968f3793fb51b783dc1
Anh Nguyen <114029945+Akari2605@users.noreply.github.com>
2023-11-30 15:36:55 -0500

    Add files via upload

---
+++ Entity/smartCandleStick.cs
@@ -0,0 +1,60 @@
+﻿using Project_2.Entity;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolTip;
+
+namespace Project_2.Entity
+{
+    //create a new smartCandleStick object
+    public class smartCandleStick : aCandleStick
+    {
+        //initial properties for the object
+        public decimal range { get; set; }
+        public decimal bodyRange { get; set; }
+        public decimal topPrice { get; set; }
+        public decimal bottomPrice { get; set; }
+        public decimal topTail { get; set; }
+        public decimal bottomTail { get; set; }
+        public bool isBullish { get; set; }
+        public bool isMarubozu { get; set; }
+        public bool isBearish { get; set; }
+        public bool isNeutral { get; set; }
+        public bool isDoji { get; set; }
+        public bool isDragonFlyDoji { get; set; }
+        public bool isGravestoneDoji { get; set; }
+        public bool isHammer { get; set; }
+        public bool isInvertedHammer { get; set; }
+
+        //constructor for smartCandleStick object that takes in an array of values as input
+        public smartCandleStick(string[] vals) : base(vals)
+        {
+            //calculate candlesticks attributes based on input values
+            range = high - low;
+            bodyRange = Math.Abs(close - open);
+            topPrice = Math.Max(open, close);
+            bottomPrice = Math.Min(open, close);
+            topTail = high - Math.Max(open, close);
+            bottomTail = Math.Min(open, close) - low;
+
+            //determine candlestick types based on the result of the above calulation
+            isBullish = close > open;
+            isBearish = open > close;
+            isNeutral = open == close;
+
+            //calculate ratios for upper and lower stick
+            double lowerShadowRatio = (double)(close - low) / (double)range;
+            double upperShadowRatio = (double)(high - close) / (double)range;
+
+            isDoji = bodyRange <= range * 0.05m;
+            isMarubozu = (bodyRange / range) >= 0.95m && topTail == 0 && bottomTail == 0;
+            isDragonFlyDoji = lowerShadowRatio >= 0.98 && upperShadowRatio < 0.02;
+            isGravestoneDoji = open == close && close == high;
+            isHammer = bottomTail > range * 0.6m && topTail < range * 0.1m;
+            isInvertedHammer = topTail > range * 0.6m && bottomTail < range * 0.1m;
+        }
+    }
+}
---
+++ LoadForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Project_2
+{
+    partial class LoadForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startDate = new System.Windows.Forms.DateTimePicker();
+            this.endDate = new System.Windows.Forms.DateTimePicker();
+            this.start_date = new System.Windows.Forms.Label();
+            this.end_date = new System.Windows.Forms.Label();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.openFileDialog_stockUpload = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // startDate
+            // 
+            this.startDate.Location = new System.Drawing.Point(232, 60);
+            this.startDate.Name = "startDate";
+            this.startDate.Size = new System.Drawing.Size(243, 22);
+            this.startDate.TabIndex = 0;
+            this.startDate.Value = new System.DateTime(2022, 9, 11, 0, 0, 0, 0);
+            // 
+            // endDate
+            // 
+            this.endDate.Location = new System.Drawing.Point(232, 106);
+            this.endDate.Name = "endDate";
+            this.endDate.Size = new System.Drawing.Size(243, 22);
+            this.endDate.TabIndex = 1;
+            // 
+            // start_date
+            // 
+            this.start_date.AutoSize = true;
+            this.start_date.Location = new System.Drawing.Point(140, 65);
+            this.start_date.Name = "start_date";
+            this.start_date.Size = new System.Drawing.Size(66, 16);
+            this.start_date.TabIndex = 2;
+            this.start_date.Text = "Start Date";
+            // 
+            // end_date
+            // 
+            this.end_date.AutoSize = true;
+            this.end_date.Location = new System.Drawing.Point(140, 111);
+            this.end_date.Name = "end_date";
+            this.end_date.Size = new System.Drawing.Size(63, 16);
+            this.end_date.TabIndex = 3;
+            this.end_date.Text = "End Date";
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(232, 165);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(152, 53);
+            this.loadButton.TabIndex = 6;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // openFileDialog_stockUpload
+            // 
+            this.openFileDialog_stockUpload.FileName = "openFileDialog";
+            this.openFileDialog_stockUpload.Filter = "All Stock files|*.csv|Daily Stocks|*-Day.csv|Weekly Stocks|*-Week.csv|Monthly Sto" +
+    "cks|*-Month.csv";
+            this.openFileDialog_stockUpload.InitialDirectory = "C:...\\Project_2\\Stock Data";
+            this.openFileDialog_stockUpload.Multiselect = true;
+            this.openFileDialog_stockUpload.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_stockUpload_FileOk);
+            // 
+            // LoadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 292);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.end_date);
+            this.Controls.Add(this.start_date);
+            this.Controls.Add(this.endDate);
+            this.Controls.Add(this.startDate);
+            this.Name = "LoadForm";
+            this.Text = "Load Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker startDate;
+        private System.Windows.Forms.DateTimePicker endDate;
+        private System.Windows.Forms.Label start_date;
+        private System.Windows.Forms.Label end_date;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog_stockUpload;
+    }
+}
+
